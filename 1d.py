0e8ceaf2147614de8e032320d2d5479acf99f299
salvis <sjansons2003@gmail.com>
2025-02-10 20:27:55 +0200

    new approach

--- 1d.py
+++ 1d.py
@@ -1,30 +1,19 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
-def plot_polygon(polygon_dict, key):
-    if key in polygon_dict:
-        coordinates = polygon_dict[key]
-        x, y = zip(*coordinates)  # Unzip coordinates into x and y lists
-        x += (x[0],)  # Close the polygon by adding the first point again
-        y += (y[0],)  # Close the polygon by adding the first point again
+# Generate polygons by giving n which is a number and func return list with tuplets
+def generate_polygon(n):
+    r = 4
+    polygon_points = []
+    angle_deg = 360 / n
+    for i in range (n):
+        angle = ( angle_deg / n ) * i
 
-        plt.plot(x, y, marker='o')
-        plt.fill(x, y, alpha=0)  
-        plt.title(f"{key.capitalize()} Polygon")
-        plt.show()
-    else:
-        print(f"Key '{key}' not found in the dictionary.")
+    return polygon_points
+
 
 def main():
-    polygon_dict = {
-        "triangle": [(0, 0), (2, 0), (1, 1)],  
-        "quadrilateral": [(0, 0), (2, 0), (2, 2), (0, 2)],
-        "pentagon": [(0, 0), (1, 0), (1.5, 1), (0.5, 2), (-0.5, 1)],
-        "hexagon": [(0, 0), (1, 0), (1.5, 1), (1,2), (0, 2),(-0.5, 1)],
-        "heptagon": [(0, 0), (1, 0), (1.5, 1), (1,2.5), (0.5, 3),(0, 2.5),(-0.5, 1)],
-        "octagon": [(0, 0), (1, 0), (1.5, 0.5), (1.5, 1.5), (1, 2), (0, 2), (-0.5, 1.5), (-0.5, 0.5)],
-        "nonagon": [(0, 0), (1, 0), (1.5, 0.5), (1.5, 1.5), (1, 2), (0, 2), (-0.5, 1.5), (-0.5, 0.5), (-1, 0)]
-    }
-    plot_polygon(polygon_dict=polygon_dict, key="heptagon")
+    polygon_points = generate_polygon(2)
 
 if __name__ == "__main__":
     main()
